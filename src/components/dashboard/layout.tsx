ee9f857b8a6dcbd3bfa745c420652f7fa25f5bba
monsterooo <createthink.net@gmail.com>
2024-08-22 17:23:28 +0800

    feat: clean Layout component

--- src/components/dashboard/layout.tsx
+++ src/components/dashboard/layout.tsx
@@ -1,13 +1,6 @@
 "use client"
 
-import {
-  createContext,
-  forwardRef,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from "react"
+import { createContext, useEffect, useRef, useState } from "react"
 
 import { cn } from "@/lib/utils"
 
@@ -20,7 +13,7 @@ interface LayoutProps extends React.HTMLAttributes<HTMLDivElement> {
   fixed?: boolean
 }
 
-const Layout = ({ className, fixed = false, ...props }: LayoutProps) => {
+export const Layout = ({ className, fixed = false, ...props }: LayoutProps) => {
   const divRef = useRef<HTMLDivElement>(null)
   const [offset, setOffset] = useState(0)
 
@@ -51,32 +44,3 @@ const Layout = ({ className, fixed = false, ...props }: LayoutProps) => {
     </LayoutContext.Provider>
   )
 }
-Layout.displayName = "Layout"
-
-const Body = forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => {
-    // Check if Layout.Body is used within Layout
-    const contextVal = useContext(LayoutContext)
-    if (contextVal === null) {
-      throw new Error(`Layout.Body must be used within ${Layout.displayName}.`)
-    }
-
-    return (
-      <div
-        ref={ref}
-        data-layout="body"
-        className={cn(
-          "px-4 py-6 md:overflow-hidden md:px-8",
-          contextVal && contextVal.fixed && "flex-1",
-          className
-        )}
-        {...props}
-      />
-    )
-  }
-)
-Body.displayName = "Body"
-
-Layout.Body = Body
-
-export { Layout }
